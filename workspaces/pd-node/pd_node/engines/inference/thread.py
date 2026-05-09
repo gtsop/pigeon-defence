10dc8d943507b781b3d3dd25f382e28d8c7fc6cc
gtsop <gtsop+github@protonmail.com>
2026-05-09 11:43:49 +0300

    perf: compare to last frame to skip inference when not needed

--- workspaces/pd-node/pd_node/engines/inference/thread.py
+++ workspaces/pd-node/pd_node/engines/inference/thread.py
@@ -1,12 +1,14 @@
 import os
 import time
 
-from ultralytics import YOLO
 from pd_node.utils import get_base_path
+from ultralytics import YOLO
+import numpy as np
 
 model = YOLO(get_base_path() / "models/yolo11n_ncnn_model", task="detect")
 
 def thread(state):
+    last_frame = None
 
     state.inference.set_names(model.names)
 
@@ -23,10 +25,18 @@ def thread(state):
             time.sleep(1/15)
             continue
 
+        if np.array_equal(frame, last_frame):
+            log("same frame, skipping")
+            time.sleep(1/15)
+            continue
+
+        last_frame = frame
+
         results = model(frame, verbose=False)[0]
 
         state.inference.set_results(results)
 
         time.sleep(1/15)
 
-
+def log(msg):
+    print("[inference_thread]:" + msg)
